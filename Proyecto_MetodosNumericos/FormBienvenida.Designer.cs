49941944c550da65229a06b210f014a08f363962
erickmayorquin2006@gmail.com <erickmayorquin2006@gmail.com>
2026-06-23 00:13:13 -0600

    Interfaz

--- Proyecto_MetodosNumericos/FormBienvenida.Designer.cs
+++ Proyecto_MetodosNumericos/FormBienvenida.Designer.cs
@@ -31,7 +31,12 @@
             btnEntrar = new Button();
             pictureBox1 = new PictureBox();
             label1 = new Label();
+            panel1 = new Panel();
+            label2 = new Label();
+            panel2 = new Panel();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
             SuspendLayout();
             // 
             // btnEntrar
@@ -39,11 +44,11 @@
             btnEntrar.BackColor = Color.LightSkyBlue;
             btnEntrar.Cursor = Cursors.Hand;
             btnEntrar.FlatStyle = FlatStyle.Flat;
-            btnEntrar.Font = new Font("Comic Sans MS", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnEntrar.Font = new Font("Comic Sans MS", 16F, FontStyle.Bold, GraphicsUnit.Point, 0);
             btnEntrar.ForeColor = Color.Black;
-            btnEntrar.Location = new Point(467, 494);
+            btnEntrar.Location = new Point(207, 516);
             btnEntrar.Name = "btnEntrar";
-            btnEntrar.Size = new Size(217, 64);
+            btnEntrar.Size = new Size(300, 86);
             btnEntrar.TabIndex = 0;
             btnEntrar.Text = "ENTRAR";
             btnEntrar.UseVisualStyleBackColor = false;
@@ -53,9 +58,9 @@
             // 
             pictureBox1.BackColor = Color.Transparent;
             pictureBox1.Image = Properties.Resources.Logo_Circular_IteraCore_1;
-            pictureBox1.Location = new Point(403, 97);
+            pictureBox1.Location = new Point(131, 21);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(342, 301);
+            pictureBox1.Size = new Size(456, 393);
             pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox1.TabIndex = 1;
             pictureBox1.TabStop = false;
@@ -64,13 +69,47 @@
             // 
             label1.AutoSize = true;
             label1.BackColor = Color.Transparent;
-            label1.Font = new Font("Comic Sans MS", 16F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Font = new Font("Comic Sans MS", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label1.ForeColor = Color.White;
-            label1.Location = new Point(300, 413);
+            label1.Location = new Point(24, 438);
             label1.Name = "label1";
-            label1.Size = new Size(561, 45);
+            label1.Size = new Size(631, 50);
             label1.TabIndex = 2;
             label1.Text = "¡Resuelve tus ejercicios en un 2x3!";
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Transparent;
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(panel2);
+            panel1.Dock = DockStyle.Fill;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1689, 788);
+            panel1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            label2.Anchor = AnchorStyles.Bottom;
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Comic Sans MS", 16F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(3, 644);
+            label2.Name = "label2";
+            label2.Size = new Size(620, 135);
+            label2.TabIndex = 4;
+            label2.Text = "Desarrollado por:\r\nIng. Antonella Franchesca Davila Cano\r\nIng. Erick Lenin Mayorquin Martinez\r\n";
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(btnEntrar);
+            panel2.Controls.Add(label1);
+            panel2.Controls.Add(pictureBox1);
+            panel2.Location = new Point(512, 12);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(695, 626);
+            panel2.TabIndex = 3;
             // 
             // FormBienvenida
             // 
@@ -80,19 +119,23 @@
             BackColor = SystemColors.Control;
             BackgroundImage = Properties.Resources.Matematica_Fondo_11;
             BackgroundImageLayout = ImageLayout.Stretch;
-            ClientSize = new Size(1117, 599);
-            Controls.Add(label1);
-            Controls.Add(pictureBox1);
-            Controls.Add(btnEntrar);
-            FormBorderStyle = FormBorderStyle.None;
+            ClientSize = new Size(1689, 788);
+            Controls.Add(panel1);
             MaximizeBox = false;
             MinimizeBox = false;
             Name = "FormBienvenida";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "FormBienvenida";
+            WindowState = FormWindowState.Maximized;
+            FormClosed += FormBienvenida_FormClosed;
+            Load += FormBienvenida_Load;
+            Resize += FormBienvenida_Resize;
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
@@ -100,5 +143,8 @@
         private Button btnEntrar;
         private PictureBox pictureBox1;
         private Label label1;
+        private Panel panel1;
+        private Panel panel2;
+        private Label label2;
     }
 }
--- Proyecto_MetodosNumericos/FormBienvenida.cs
+++ Proyecto_MetodosNumericos/FormBienvenida.cs
@@ -14,7 +14,13 @@ namespace Proyecto_MetodosNumericos
         {
             InitializeComponent();
             btnEntrar.FlatAppearance.BorderSize = 0;
+
+            // Para centrar
+            panel2.Anchor = AnchorStyles.None;
+            this.Resize += new EventHandler(FormBienvenida_Resize);
         }
+
+
 
         private void btnEntrar_Click(object sender, EventArgs e)
         {
@@ -22,6 +28,36 @@ namespace Proyecto_MetodosNumericos
             this.Hide();
             loginForm.ShowDialog();
         }
-        
+
+        private void FormBienvenida_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        // TODO ESTO ES PARA CENTRAR, ANTONELLA
+        private void FormBienvenida_Resize(object sender, EventArgs e)
+        {
+            CentrarPanelContenedor();
+        }
+
+        private void FormBienvenida_Load(object sender, EventArgs e)
+        {
+            CentrarPanelContenedor();
+        }
+
+        private void CentrarPanelContenedor()
+        {
+            // Suponiendo que panel2 está directamente sobre el Formulario:
+            // Usamos ClientSize para tomar el área visible real de la ventana
+            int x = (panel1.Width - panel2.Width) / 2;
+            int y = (panel1.ClientSize.Height - panel2.Height) / 2;
+
+            panel2.Location = new Point(x, y);
+
+            /* * NOTA IMPORTANTE: 
+             * Si metiste el 'panel2' adentro del 'panel1' (en lugar del formulario), 
+             * cambia "this.ClientSize.Width" por "panel1.Width" y lo mismo para el Height.
+             */
+        }
     }
 }
